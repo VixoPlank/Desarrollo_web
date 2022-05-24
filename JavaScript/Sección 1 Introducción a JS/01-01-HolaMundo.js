27e146e6821b7a4166dfc63d5698fe3125f2aad3
Vicente Vásquez <103214246+VixoPlank@users.noreply.github.com>
2022-05-24 13:51:21 -0400

    Add files via upload

---
+++ JavaScript/Sección 1 Introducción a JS/01-01-HolaMundo.js
@@ -0,0 +1,3 @@
+var nombre = "Vicente";
+
+console.log(nombre);
---
+++ JavaScript/Sección 1 Introducción a JS/02-01-TiposDatosJavaScript-UJS.js
@@ -0,0 +1,20 @@
+/*
+Ejemplos de 
+tipos de datos 
+en JavaScript
+*/
+//Tipo de dato string
+var nombre = "Carlos";
+console.log(nombre);
+
+//Tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+var objecto = {
+    nombre : "Juan",
+    apellido : "Perez",
+    telefono : "55443322"
+}
+console.log(objecto);
