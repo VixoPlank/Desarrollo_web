37811c61b1d2423e62ef71d1689eb8126324cf57
Vicente Vásquez <103214246+VixoPlank@users.noreply.github.com>
2022-05-24 19:09:01 -0400

    Update 02-07-VarLetConst-JavaScript.js

--- JavaScript/Sección 2 Tipos de datos en JS/02-07-VarLetConst-JavaScript.js
+++ JavaScript/Sección 2 Tipos de datos en JS/02-07-VarLetConst-JavaScript.js
@@ -1,3 +1,7 @@
+// Las variables var pueden ser modificadas y re-declaradas dentro de su ámbito.
+// Las variables let pueden ser modificadas, pero no re-declaradas.
+// Las variables const no pueden ser modificadas ni re-declaradas.
+
 let nombre;
 nombre = "Juan";
 console.log( nombre );
